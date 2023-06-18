8efdd9d8b30cfa3c00657a381120d64a36a2ea5d
Akshavya Aggarwal <97899039+Akshavya2509@users.noreply.github.com>
2023-06-18 14:41:56 +0530

    Create 29. AddTwoNumbers.cpp

---
+++ 29. AddTwoNumbers.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition of linked list:
+ *
+ * class Node {
+ * public:
+ *      int data;
+ *      Node *next;
+ *      Node() {
+ *          this->data = 0;
+ *          this->next = NULL;
+ *      }
+ *      Node(int data) {
+ *          this->data = data;
+ *          this->next = NULL;
+ *      }
+ *      Node (int data, Node *next) {
+ *          this->data = data;
+ *          this->next = next;
+ *      }
+ * };
+ *
+ *************************************************************************/
+
+Node* addTwoNumbers(Node* num1, Node* num2) {
+    if (num1 == nullptr) {
+        return num2;
+    }
+
+    if (num2 == nullptr) {
+        return num1;
+    }
+
+    Node* dummy = new Node(0);
+    Node* curr = dummy;
+    int carry = 0;
+
+    while (num1 != nullptr || num2 != nullptr) {
+        int sum = carry;
+
+        if (num1 != nullptr) {
+            sum += num1->data;
+            num1 = num1->next;
+        }
+
+        if (num2 != nullptr) {
+            sum += num2->data;
+            num2 = num2->next;
+        }
+
+        curr->next = new Node(sum % 10);
+        curr = curr->next;
+        carry = sum / 10;
+    }
+
+    if (carry > 0) {
+        curr->next = new Node(carry);
+    }
+
+    return dummy->next;
+}
